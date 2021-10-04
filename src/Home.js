02ed41999a9823af4cd7a61f8fef1bdd92f0db60
Aby Joseph <abyjosef.git@gmail.com>
2021-10-04 22:48:31 +0530

    Upto Lession 10
    Upto Lession 10

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,30 @@
+import { useState } from 'react';
+
 const Home = () => {
+
+const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+]);
+
     const handleClick = (e) => {
-        console.log(e);
+        
     }
-    const handleClickAgain = (name, e) => {
-        console.log(e.target);
-    }
+
     return ( 
         <div className="home">
-            <h1>Home Page</h1>
-             <button onClick={handleClick}>Click Me</button>
-             <button onClick={(e)=>{handleClickAgain('aby', e)}}>Click Me Again</button>
+        {blogs.map(blog=>(
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>{blog.body}</p>
+                <span>{blog.author}</span>
+            </div>
+
+        ))}
         </div>
+
+        
      );
 }
  
