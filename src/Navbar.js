c607dc0e92be5418cd1ba59d0c18a823f92ea1d6
Aby Joseph <abyjosef.git@gmail.com>
2021-10-06 21:00:51 +0530

    Route, Route Link, Cleanup to abort unwanted fetch

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-        <h1>The DOJO blog</h1>
-        <div className="links">
-            <a href="/">Home</a>
-            <a href="/Create" >New Blog</a>
-        </div>
-        </nav>
+import {Link} from "react-router-dom"
 
-      );
-}
- 
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The DOJO blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/Create">New Blog</Link>
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,34 +1,37 @@
 import { useState, useEffect } from "react";
-const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-    useEffect(()=> {
 
-        setTimeout(() => {
-      
-         fetch(url) 
-         .then(res => {
-           console.log(res);
-             if(!res.ok)
-             {
-               throw Error("Something went wrong");
-             }
-             return res.json();
-          })
-         .then(data =>{
-             setData(data);
-             setIsPending(false);
-             setError(null);
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  const abortCtrl = new AbortController();
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(url, { signal: abortCtrl.signal })
+        .then((res) => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error("Something went wrong");
           }
-         )
-         .catch(err => {
-             setError(err.message);
-             setIsPending(false);
-         })
-      }, 1000);
-      }, [url])
-    return ({data, isPending, error}  );
-}
- 
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if (err.name === "AbortError") {
+            console.log("Fetch Aborted");
+          } else {
+            setError(err.message);
+            setIsPending(false);
+          }
+        });
+    }, 1000);
+    return () => abortCtrl.abort();
+  }, [url]);
+  return { data, isPending, error };
+};
+
 export default useFetch;
