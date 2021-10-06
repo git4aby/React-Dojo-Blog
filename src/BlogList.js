0fd3a42b6d62b856d3f2ae77266cb2bebb293713
Aby Joseph <abyjosef.git@gmail.com>
2021-10-06 20:16:19 +0530

    Custom hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -5,9 +5,9 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         {blogs.map(blog => (
           <div className="blog-preview" key={blog.id} >
             <h2>{ blog.title }</h2>
-            <p>{blog.body}</p>
+            {/* <p>{blog.body}</p> */}
             <p>Written by { blog.author }</p>
-            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+            {/* <button onClick={() => handleDelete(blog.id)}>delete blog</button> */}
           </div>
         ))}
       </div>
--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,19 @@
-import { useState, useEffect } from "react";
+
 import BlogList from "./BlogList";
+import useFetch from './useFetch'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
-
-
-  const handleDelete = (id) => {
+/*   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
-  }
-
-
-    
-
-
-useEffect(()=> {
-
-    setTimeout(() => {
-
-   fetch('http://localhost:8000/blogs') 
-   .then(res => {return res.json();})
-   .then(data =>{
-       setBlogs(data);
-       setIsPending(false);
-    }
-   )
-}, 1000);
-}, [])
-
+  } */
   return (
     <div className="home">
+    {error && <div>{error}</div>}
     {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />}
+    {blogs && <BlogList blogs={blogs} title="All Blogs" />}
       
     </div>
   );
